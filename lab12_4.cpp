217a00376ed043dc0eebfe4aa791a9ec042543a1
Tanida <printer.rung@gmail.com>
2024-12-24 15:55:09 +0700

    lab12_4 completed

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 
 using namespace std;
+template <typename swapping>
+void mySwap(swapping &x,swapping &y); 
 
 int main(){
 	int x, y;
@@ -30,3 +32,9 @@ int main(){
 	return 0;
 }
 
+template <typename swapping>
+void mySwap(swapping &x,swapping &y){
+    swapping nun = x;
+    x=y;
+    y=nun;
+}
